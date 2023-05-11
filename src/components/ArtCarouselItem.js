e472a9d12a926daf1b594c5e58652e2a6a7dbed6
Jay Wiggins <5948+funkfinger@users.noreply.github.com>
2023-05-11 09:16:33 -0700

    working on carousel

--- src/components/ArtCarouselItem.js
+++ src/components/ArtCarouselItem.js
@@ -1,17 +1,24 @@
+import Container from "react-bootstrap/Container";
 import Carousel from "react-bootstrap/Carousel";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const ArtCarouselItem = () => (
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/images/unnamed.png"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>first slide caption</p>
-    </Carousel.Caption>
-  </Carousel.Item>
+  <Container fluid>
+    <Carousel.Item>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="/images/screenshot1.png" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </Carousel.Item>
+  </Container>
 );
 
 export default ArtCarouselItem;
