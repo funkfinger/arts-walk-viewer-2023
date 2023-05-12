31c893afa88e2f8e4c656a044350d53aff477d5b
Jay Wiggins <5948+funkfinger@users.noreply.github.com>
2023-05-11 19:04:35 -0700

    updates

--- src/components/ArtCarousel.js
+++ src/components/ArtCarousel.js
@@ -1,61 +1,63 @@
 import Container from "react-bootstrap/Container";
-import Carousel from "react-bootstrap/Carousel";
+// import Carousel from "react-bootstrap/Carousel";
+import Carousel from "react-multi-carousel";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+
+import "react-multi-carousel/lib/styles.css";
 
 import { ProjectData } from "../data/data";
 
 console.log(ProjectData);
 
-const ArtCarousel = () => (
-  <Container fluid>
-    <Carousel interval={3000}>
-      {ProjectData.map((project) => {
-        return (
-          <Carousel.Item>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="/images/screenshot1.png" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Carousel.Item>
-        );
-      })}
-      {/* <Carousel.Item>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="/images/screenshot1.png" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Carousel.Item>
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
-      <Carousel.Item>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="/images/screenshot1.png" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Carousel.Item> */}
-    </Carousel>
-  </Container>
-);
+const ArtCarousel = () => {
+  return (
+    <Container fluid>
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        transitionDuration={3000}
+        autoPlay={true}
+        centerMode={true}
+      >
+        {ProjectData.map((project) => {
+          return (
+            <div>
+              <Card style={{ width: "16rem" }}>
+                <Card.Img variant="top" src={project.img} />
+                <Card.Body>
+                  <Card.Title>{project.title}</Card.Title>
+                  <Card.Subtitle>{project.artist}</Card.Subtitle>
+                  <Card.Text>{project.description}</Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </div>
+          );
+        })}
+      </Carousel>
+    </Container>
+  );
+};
 
 export default ArtCarousel;
